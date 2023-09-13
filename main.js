663d488e176923a784c1c5c14fa5e1c6537efc78
Wali7719 <128316841+Wali7719@users.noreply.github.com>
2023-09-13 18:31:36 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function startclassification()
+ {
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/EVJWs0ERg/model.json', modelReady);
+ }
+ function modelReady()
+ {
+    classifier.classify(gotResults);
+ }
